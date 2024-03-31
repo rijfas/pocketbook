68485a41ea5471aee5eadef59599ecbb0c1254b7
rijfas <rijfas01@gmail.com>
2024-03-31 15:43:04 +0530

    added segmented button in transactions page

--- lib/presentation/screens/dashboard/pages/transactions.dart
+++ lib/presentation/screens/dashboard/pages/transactions.dart
@@ -16,6 +16,26 @@ class _TransactionsPageState extends State<TransactionsPage> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
+          SegmentedButton(
+            showSelectedIcon: false,
+            onSelectionChanged: (p0) => {},
+            segments: [
+              ButtonSegment(
+                value: 'weekly',
+                label: Text('Weekly'),
+              ),
+              ButtonSegment(
+                value: 'monthly',
+                label: Text('Monthly'),
+              ),
+              ButtonSegment(
+                value: 'yearly',
+                label: Text('Yearly'),
+              ),
+            ],
+            selected: {'weekly'},
+          ),
+          SizedBox(height: 36),
           SizedBox(
             width: MediaQuery.of(context).size.width,
             height: 200,
@@ -44,56 +64,62 @@ class _TransactionsPageState extends State<TransactionsPage> {
                   BarChartGroupData(
                     barRods: [
                       BarChartRodData(
-                          toY: 200, width: 16, color: Colors.grey[200])
+                          toY: 200, width: 16, color: Colors.blue[300])
                     ],
                     x: 1,
                   ),
                   BarChartGroupData(
                     barRods: [
                       BarChartRodData(
-                          toY: 300, width: 16, color: Colors.grey[200])
+                          toY: 300, width: 16, color: Colors.blue[300])
                     ],
                     x: 2,
                   ),
                   BarChartGroupData(
                     barRods: [
                       BarChartRodData(
-                          toY: 500, width: 16, color: Colors.grey[200])
+                          toY: 500, width: 16, color: Colors.blue[300])
                     ],
                     x: 3,
                   ),
                   BarChartGroupData(
                     barRods: [
                       BarChartRodData(
-                          toY: 600, width: 16, color: Colors.grey[200])
+                          toY: 600, width: 16, color: Colors.blue[300])
                     ],
                     x: 4,
                   ),
                   BarChartGroupData(
                     barRods: [
                       BarChartRodData(
-                          toY: 300, width: 16, color: Colors.grey[200])
+                          toY: 300, width: 16, color: Colors.blue[300])
                     ],
                     x: 5,
                   ),
                   BarChartGroupData(
                     barRods: [
                       BarChartRodData(
-                          toY: 400, width: 16, color: Colors.grey[200])
+                          toY: 400, width: 16, color: Colors.blue[300])
                     ],
                     x: 6,
                   ),
                   BarChartGroupData(
                     barRods: [
                       BarChartRodData(
-                          toY: 100, width: 16, color: Colors.grey[200])
+                          toY: 100, width: 16, color: Colors.blue[300])
                     ],
                     x: 7,
                   ),
                 ],
               ),
             ),
-          )
+          ),
+          SizedBox(height: 24),
+          Text(
+            'Categories',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+          ),
+          Divider(),
         ],
       ),
     );
