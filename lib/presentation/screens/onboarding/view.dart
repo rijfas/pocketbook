87ddcfe55201a8055821935b72a2ba610e76e931
rijfas <rijfas01@gmail.com>
2024-04-14 20:08:11 +0530

    removed unwanted imports

--- lib/presentation/screens/onboarding/view.dart
+++ lib/presentation/screens/onboarding/view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:lottie/lottie.dart';
 import 'package:pocketbook/presentation/screens/onboarding/controller.dart';
 import 'package:pocketbook/utils/assets.dart';
