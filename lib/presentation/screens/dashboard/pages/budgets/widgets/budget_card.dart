9136f03850a3f66282b462954c2f888c15d5a32e
rijfas <rijfas01@gmail.com>
2024-04-29 10:46:09 +0530

    fixed budget card

--- lib/presentation/screens/dashboard/pages/budgets/widgets/budget_card.dart
+++ lib/presentation/screens/dashboard/pages/budgets/widgets/budget_card.dart
@@ -32,43 +32,73 @@ class _BudgetCardState extends State<BudgetCard> {
   @override
   Widget build(BuildContext context) {
     return Card(
+      margin: const EdgeInsets.symmetric(vertical: 8.0),
       child: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Stack(
-              children: [
-                SizedBox(
-                  height: 96,
-                  width: 96,
-                  child: CircularProgressIndicator(
-                    value: _spentAmount / widget.budget.amount,
-                    backgroundColor: Colors.grey[400],
-                    strokeWidth: 6,
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        SizedBox(
+                          height: 96,
+                          width: 96,
+                          child: CircularProgressIndicator(
+                            value: _spentAmount / widget.budget.amount,
+                            backgroundColor: Colors.grey[400],
+                            strokeWidth: 6,
+                          ),
+                        ),
+                        Text(
+                          '${NumberFormat.compact().format(
+                            _spentAmount * 100 / widget.budget.amount,
+                          )}%',
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                Text(
-                  '${NumberFormat.compact().format(
-                    _spentAmount * 100 / widget.budget.amount,
-                  )}%',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 24,
-                  ),
-                ),
-              ],
-              alignment: Alignment.center,
-            ),
-            const SizedBox(height: 16),
-            Text(
-              '${Formatters.dateWithYearMonth(widget.budget.startDate)} - ${Formatters.dateWithYearMonth(widget.budget.endDate)}',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          Formatters.dateWithYearMonth(widget.budget.startDate),
+                          style: const TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const Text(
+                          'to',
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 18.0,
+                          ),
+                        ),
+                        Text(
+                          Formatters.dateWithYearMonth(widget.budget.endDate),
+                          style: const TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
               ),
             ),
-            const SizedBox(height: 8),
+            const SizedBox(height: 16),
             const Divider(),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
